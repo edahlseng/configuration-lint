82d61ec8f42ca8ef74998b686cc5565354bb6b66
Eric Dahlseng <edahlseng@yahoo.com>
2018-11-11 11:37:23 -0800

    feat: Add yaml

--- sources/prettier.js
+++ sources/prettier.js
@@ -1,9 +1,8 @@
 /* @flow */
 
 import path from "path";
-import { always } from "ramda";
 
-import { addNpmScript, setupConfigurationFile } from "./utils.js";
+import { setupConfigurationFile } from "./utils.js";
 
 const prettierConfiguration = `module.exports = require('@eric.dahlseng/linter-configuration/prettierrc');\n`;
 
@@ -11,24 +10,6 @@ const setupPrettier = (projectRootDirectory: string) =>
 	setupConfigurationFile({
 		configuration: prettierConfiguration,
 		filePath: path.resolve(projectRootDirectory, "./.prettierrc.js"),
-	}).chain(
-		always(
-			addNpmScript({
-				packageJsonPath: path.resolve(projectRootDirectory, "./package.json"),
-				name: "lint:json",
-				content:
-					"prettier --config .prettierrc.js --list-different '**/*.json'",
-			})
-		)
-	).chain(
-		always(
-			addNpmScript({
-				packageJsonPath: path.resolve(projectRootDirectory, "./package.json"),
-				name: "lint-report:json",
-				content:
-					"mkdir -p ./linting-results/prettier-json && prettier-junit --config .prettierrc.js '**/*.json' > ./linting-results/prettier-json/report.xml"
-				})
-			)
-		);
+	});
 
 export default setupPrettier;
